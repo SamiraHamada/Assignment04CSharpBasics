612e24fbc388316aafc37149b9c5671a8e4b6a53
samira hamada <samirahamadaabdelaleem123@gmail.com>
2026-09-20 11:41:06 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignment04CSharpBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#region 1st Question
+            ////Add a private string password = "secret"; field to a Book class. Try to print it from Main  (outside the class). What happens, and why?
+            //Book book = new Book();
+
+            //Console.WriteLine(book.password); // This will cause a compilation error because private members can only be accessed inside the same class. 
+            //#endregion
+        }
+    }
+}
